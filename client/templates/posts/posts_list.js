78b1373e8d581a58b5d16e14def05666c49df88d
Dominic Ho <domtkho@gmail.com>
2014-11-02 22:30:03 +0800

    Wired collection into postList template

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,20 +1,5 @@
-var postsData = [
-  {
-    title: "Introducing Telescope",
-    url: "http://sachagrief.com/indtroducing-telescope"
-  },
-
-  {
-    title: "Meteor",
-    url: "http://meteor.com"
-  },
-
-  {
-    title: "The Meteor Book",
-    url: "http://themeteorbook.com"
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
-})
+  posts: function() {
+    return Posts.find();
+  }
+});
